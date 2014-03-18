9f8827388cefbb004d91a6f2043eaeef3a5304c0
W. Ryan Walker <ryan.walker@lazarussoftware.com>
2014-03-17 20:52:38 -0500

    working implementation that supports a routing table plus individual class-like objects to handle the respective calls.

--- web.js
+++ web.js
@@ -1,60 +1,37 @@
 // web.js
 var restify = require("restify");
-
 var logfmt = require("logfmt");
 var routes = require('routes');
-
-var query = require('pg-query');
-query.connectionParameters = 'postgress://xrikhvzgzccucq:U4wdycYpgITncBNcT49eXhiEUE@ec2-54-204-43-200.compute-1.amazonaws.com:5432/dcp9eo51skot7s?ssl=true';
-
-function respond(request, response, next) {
-	response.send('hello ' + request.params.name);
-}
 
 var server = restify.createServer({
   name: 'api.localboards.org'
 });
+
+// keep curl from retaining the connection with 'keep-alive' setting.
+// could be useful in other applications, but not for this one.
 server.pre(restify.pre.userAgentConnection());
 
+//
+// setup all of the routes that this api will listen for.
 for(var ndx=0;ndx < routes.paths.length;ndx++) {
 	var resource = routes.paths[ndx];
 	console.log( 'resource: ' + resource.name );
-	// var resourcePath = new RegExp('^\/' + resource.name + '(\/[A-Z]{2})*'/* '(\/([a-zA-Z0-9_\.~-]+)*)*' */);
-	// console.log( ' == main path: ' + resourcePath);
 	for(var index=0;index<resource.resources.length;index++) {
 		var defn = resource.resources[index];
 		for(var udx=0;udx<defn.uris.length;udx++) {
 			switch(defn.httpMethod) {
 				case 'GET': {
-					server.get('/'+defn.uris[udx].uri, eval(defn.uris[udx].method)/* routes.dispatch */);
+					server.get('/'+defn.uris[udx].uri, defn.uris[udx].method); // routes.dispatch
 					console.log('added ' + defn.uris[udx].uri);
 				} break;
 			}
 			console.log(' - ' + defn.httpMethod + 
 						': ' + defn.uris[udx].uri +
-						': handler - ' + defn.uris[udx].method + 
-						'(' + JSON.stringify(defn.uris[udx].args) + ')');
+						': handler(' + JSON.stringify(defn.uris[udx].args) + ')');
 		}
 	}
 }
 
-// server.get('/hello/:name', respond);
-// server.head('/hello/:name', respond);
-/*
-server.get('/states/:name/cities', function(req, rsp, next) {
-	console.log('params: ' + JSON.stringify(req.params));
-	
-	query("select * from states inner join cities on states.id = cities.state_id where abbreviation = $1::text",[req.params.name],
-		function(err, rows, result) {
-			console.log('Status: ' + err);
-			
-			rsp.contentType = 'json';
-			rsp.send(200, rows);
-			return next();
-		}
-	);
-});
-*/
-server.listen(8081, function() {
+server.listen(routes.port, function() {
 	console.log('%s listening at %s', server.name, server.url);
 })
